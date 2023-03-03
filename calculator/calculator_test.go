320f285b3ae5d631c229c24e0d55224613361a4a
gkspranger <greg@spranger.us>
2023-03-03 09:38:58 -0500

    ok

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -2,12 +2,43 @@ package calculator_test
 
 import (
 	"calculator"
+	"reflect"
+	"runtime"
 	"testing"
 )
 
 type testCase struct {
 	a, b float64
 	want float64
+}
+
+func GetFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+
+func RunFunctionTestCases(tcs []testCase, fn func(a, b float64) float64, t *testing.T) {
+	fnName := GetFunctionName(fn)
+	for _, tc := range tcs {
+		got := fn(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("%s(%f, %f): want %f, got %f",
+				fnName, tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func RunFunctionTestCasesWithErrors(tcs []testCase, fn func(a, b float64) (float64, error), t *testing.T) {
+	fnName := GetFunctionName(fn)
+	for _, tc := range tcs {
+		got, err := fn(tc.a, tc.b)
+		if err != nil {
+			t.Fatalf("want no error for valid input, got %v", err)
+		}
+		if tc.want != got {
+			t.Errorf("%s(%f, %f): want %f, got %f",
+				fnName, tc.a, tc.b, tc.want, got)
+		}
+	}
 }
 
 func TestAdd(t *testing.T) {
@@ -17,12 +48,7 @@ func TestAdd(t *testing.T) {
 		{a: 3, b: 2, want: 5},
 		{a: 4, b: 3, want: 7},
 	}
-	for _, tc := range testCases {
-		got := calculator.Add(tc.a, tc.b)
-		if tc.want != got {
-			t.Errorf("want %f, got %f", tc.want, got)
-		}
-	}
+	RunFunctionTestCases(testCases, calculator.Add, t)
 }
 
 func TestSubtract(t *testing.T) {
@@ -32,12 +58,7 @@ func TestSubtract(t *testing.T) {
 		{a: -3, b: -2, want: -1},
 		{a: -4, b: 3, want: -7},
 	}
-	for _, tc := range testCases {
-		got := calculator.Subtract(tc.a, tc.b)
-		if tc.want != got {
-			t.Errorf("want %f, got %f", tc.want, got)
-		}
-	}
+	RunFunctionTestCases(testCases, calculator.Subtract, t)
 }
 
 func TestMultiply(t *testing.T) {
@@ -47,10 +68,15 @@ func TestMultiply(t *testing.T) {
 		{a: -3, b: -2, want: 6},
 		{a: -4, b: 3, want: -12},
 	}
-	for _, tc := range testCases {
-		got := calculator.Multiply(tc.a, tc.b)
-		if tc.want != got {
-			t.Errorf("want %f, got %f", tc.want, got)
-		}
+	RunFunctionTestCases(testCases, calculator.Multiply, t)
+}
+
+func TestDivideValidTestCases(t *testing.T) {
+	t.Parallel()
+	testCases := []testCase{
+		{a: 2, b: 2, want: 1},
+		{a: -3, b: 1, want: -3},
+		{a: 12, b: 3, want: 4},
 	}
+	RunFunctionTestCasesWithErrors(testCases, calculator.Divide, t)
 }
