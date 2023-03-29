98107a2adffb549d295d96bd9841ffd148328f21
gkspranger <greg@spranger.us>
2023-03-29 09:10:51 -0400

    save for now

--- happyfun/mytypes/mytypes.go
+++ happyfun/mytypes/mytypes.go
@@ -1,13 +1,46 @@
 package mytypes
 
+import "strings"
+
 type MyInt int
 
 func (i MyInt) Twice() MyInt {
 	return i * 2
 }
 
+func (i *MyInt) Double() {
+	// *i *= 2
+	*i = *i * 2
+}
+
 type MyString string
 
 func (s MyString) Len() int {
 	return len(s)
+}
+
+type MyStrBuilder strings.Builder
+
+func (m MyStrBuilder) Hello() string {
+	return "Hello, Gophers!"
+}
+
+type MyBuilder struct {
+	Contents strings.Builder
+}
+
+type StringUppercaser struct {
+	Contents strings.Builder
+}
+
+func (s StringUppercaser) ToUpper() string {
+	return strings.ToUpper(s.Contents.String())
+}
+
+func Double(input int) int {
+	return input * 2
+}
+
+func DoubleP(input *int) {
+	*input *= 2
 }
--- happyfun/mytypes/mytypes_test.go
+++ happyfun/mytypes/mytypes_test.go
@@ -45,3 +45,82 @@ func TestStringsBuilder(t *testing.T) {
 			wantLen, gotLen)
 	}
 }
+
+// func TestMyBuilderLen(t *testing.T) {
+// 	t.Parallel()
+// 	var mb mytypes.MyStrBuilder
+// 	want := 15
+// 	got := mb.Len()
+// 	if want != got {
+// 		t.Errorf("want %d, got %d", want, got)
+// 	}
+// }
+
+func TestMyBuilderHello(t *testing.T) {
+	t.Parallel()
+	var mb mytypes.MyStrBuilder
+	want := "Hello, Gophers!"
+	got := mb.Hello()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func TestMyBuilder(t *testing.T) {
+	t.Parallel()
+	var mb mytypes.MyBuilder
+	mb.Contents.WriteString("Hello, ")
+	mb.Contents.WriteString("Gophers!")
+	want := "Hello, Gophers!"
+	got := mb.Contents.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+	wantLen := 15
+	gotLen := mb.Contents.Len()
+	if wantLen != gotLen {
+		t.Errorf("%q: want len %d, got %d",
+			mb.Contents.String(), wantLen, gotLen)
+	}
+}
+
+func TestStringUppercaser(t *testing.T) {
+	t.Parallel()
+	var su mytypes.StringUppercaser
+	su.Contents.WriteString("hello")
+	want := "HELLO"
+	got := su.ToUpper()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func TestDouble(t *testing.T) {
+	t.Parallel()
+	want := 4
+	got := mytypes.Double(2)
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestDoubleP(t *testing.T) {
+	t.Parallel()
+	x := 2
+	want := 4
+	mytypes.DoubleP(&x)
+	if want != x {
+		t.Errorf("want %d, got %d", want, x)
+	}
+}
+
+func TestDoubleM(t *testing.T) {
+	t.Parallel()
+	x := mytypes.MyInt(12)
+	want := mytypes.MyInt(24)
+	p := &x
+	p.Double()
+	if want != x {
+		t.Errorf("want %d, got %d", want, x)
+	}
+}
